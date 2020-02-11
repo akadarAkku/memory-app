9031c6a6d794ed0cbff4198a5443e8d3abd3103f
Akadar.akku <akadar.akku121@gmail.com>
2020-02-11 20:38:25 +0100

    Updates

---
+++ precache-manifest.0233447c1fd5239dd55af0b8017e6d93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3bb31bd9d965442f555859dff76adab",
+    "url": "/memory-app/index.html"
+  },
+  {
+    "revision": "eb2a3ac5a9805b7848d5",
+    "url": "/memory-app/static/css/main.b4f3e40f.chunk.css"
+  },
+  {
+    "revision": "a5d50a71f81cd01e2923",
+    "url": "/memory-app/static/js/2.64d7af0a.chunk.js"
+  },
+  {
+    "revision": "20b77c4f11b8ea1612799fe0d6fa407f",
+    "url": "/memory-app/static/js/2.64d7af0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2a3ac5a9805b7848d5",
+    "url": "/memory-app/static/js/main.9878e9e7.chunk.js"
+  },
+  {
+    "revision": "0ee88d81f29e39e9070a",
+    "url": "/memory-app/static/js/runtime-main.0de76911.js"
+  }
+]);
